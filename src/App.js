ff8fc8ecb493f46bd006b7cebf343a670b427dba
Aneesh <28660350+aminoa@users.noreply.github.com>
2022-05-30 14:38:08 -0400

    changed resume, fixed links

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1 className="title">Hey, I'm Aneesh Maganti 👋</h1>
         <h3 className="description">I'm currently studying CS at NYU. Check out my resume and projects here. </h3>
         
-        <h2 className="resume"> <a href="https://docs.google.com/viewer?url=https://raw.githubusercontent.com/aminoa/resume/main/resume.pdf?token=GHSAT0AAAAAABU74U3C77XTABS326JRSE2EYUVBJVA">Resume</a></h2>
+        <h2 className="resume"> <a href="https://github.com/aminoa/resume/blob/main/resume.pdf">Resume</a></h2>
 
         <h2 className="project_title">Interesting Projects</h2>
         <ul className="project_list">
@@ -16,7 +16,7 @@ function App() {
           <li><a href="https://bluebird747.itch.io/dac"> DAC: Direct Air Capture Plant Exploration Game </a></li>
           <li><a href="https://github.com/aminoa/appl"> appl: API for NYU Engage</a></li>
           <li><a href="https://github.com/bkRoad/bkRoad"> bkRoad: Book Management Website</a></li>
-          <li><a href="https://github.com/aminoa/auto-daily-screener"> Auto Daily Screener - filling out NYU daily screeners </a></li>
+          <li><a href="https://github.com/aminoa/auto-daily-screener"> Auto Daily Screener: fill NYU daily screeners without input </a></li>
         </ul> 
 
       </FadeIn>
